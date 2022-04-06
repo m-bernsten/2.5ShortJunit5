79f5878a9ec43778b6cb9482ca81f8c549574a88
m-bernsten <you@example.com>
2022-04-06 21:19:53 +0200

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+@Test
+@DisplayName("2+1 =3")
+void addsTwoNumber () {
+    Calculator calculator= new Calculator();
+    assertEquals(3, calculator.add(2, 1), "2+1 should equal 3" );
+}
+
+@Test
+    @DisplayName("2 - 1 = 1")
+    void subtractsTwoNumber () {
+    Calculator calculator = new Calculator();
+    assertEquals(1, calculator.subtract(2,1), "2-1 should equal 1");
+}
+}
